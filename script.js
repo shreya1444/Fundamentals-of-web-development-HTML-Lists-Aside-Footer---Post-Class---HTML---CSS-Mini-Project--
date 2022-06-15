021a2ec0757e4e1b15dd857ca3cc548604b8a9a1
ShreyaBajpai <101422183+shreya1444@users.noreply.github.com>
2022-06-15 13:49:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//typing Animations
+
+var typed=new Typed(".typing", {
+    strings:[" B-Tech Graduate"," Web-Developer"," Coder"],
+    typespeed:100,
+    backSpeed:60,
+    loop:true
+})
+var typed=new Typed(".typing-2", {
+    strings:["  B-Tech Graduate"," Web-Developer "," Coder"],
+    typespeed:99,
+    backSpeed:58,
+    loop:true
+
+
+})
